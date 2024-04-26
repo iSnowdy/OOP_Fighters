b11c4eab4ab5cda2f97b5fdb27c149ea41d115f9
Snow <andy0509rey@gmail.com>
2024-04-26 12:37:52 +0200

    Specific parameters reading for JSON files

---
+++ src/main/JSON Tests/JSON/JSON_04.java
@@ -0,0 +1,64 @@
+package JSON;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+public class JSON_04 {
+    public static void main (String[] args) {
+        // Like this we will be able to read an already created .json file
+        JsonObject jsonObject = null; // So we can use it inside and outside the try-catch block
+        Gson gson = null;
+        try {
+            FileReader fileReader = new FileReader("fighterstest.json");
+
+            gson = new GsonBuilder().setPrettyPrinting().create();
+            // setPrettyPrinting modifies the JSON output so that is readable to humans
+            // meaning that it will have spaces, blanks, line breaks and so on
+            jsonObject = gson.fromJson(fileReader, JsonObject.class);
+
+            System.out.println("Reading JSON...\n\n");
+            String rank = "Five";
+            for (int i = 0; i < jsonObject.getAsJsonArray("Fighters").size(); i ++) {
+                JsonObject fighter00 =jsonObject.getAsJsonArray("Fighters").get(i).getAsJsonObject();
+                if (Objects.equals(fighter00.get("Rank").getAsString(), rank)) {
+                    System.out.println("Fighter of Rank " + rank + " details are: \n");
+                    for (String key : fighter00.keySet()) {
+                        System.out.println(key + ": " + fighter00.get(key));
+                    }
+                    System.out.println();
+                }
+            }
+            fileReader.close();
+        } catch (IOException exception00) {
+            exception00.printStackTrace();
+        }
+
+        // Like this we will be able to modify existen data in a .json file. Notice the index and String names
+        // are really important
+        /*System.out.println("\nWe modify Fighter 01 Vitality from 10 -> 15\n");
+
+        JsonObject fighter01 = jsonObject.getAsJsonArray("Fighters").get(0).getAsJsonObject();
+        fighter01.addProperty("Vitality", 15);
+        System.out.println(gson.toJson(jsonObject));
+
+        // Printing specific parts of the JSON Objects inside the Fighters Array
+        String fighter01name = fighter01.get("Name").getAsString();
+        System.out.println(fighter01name);
+
+        System.out.println("-------------");
+
+        for (String key : fighter01.keySet()) {
+            System.out.println(key + ": " + fighter01.get(key));
+        }
+
+
+        // However we are not finished. We've only modified the data in jsonObject memory. The original file, outside
+        // the project, has not yet been modified*/
+
+    }
+}
